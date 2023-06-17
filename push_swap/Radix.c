9dfa567d51c706762f64c821b0d3c0fb9c396ef6
IremK <ayseirem.kayacioglu@gmail.com>
2023-06-17 23:30:57 +0300

    Omg this passes the <= 100 test

--- push_swap/Radix.c
+++ push_swap/Radix.c
@@ -15,46 +15,16 @@ void    make_positive(t_list **a)
     
 }
 
-/*
-void make_positive(t_list **a, int index)
-{
-    int min = find_min(*a);
-    t_list *temp = *a;
-
-    while (temp)
-    {
-        if (temp->content == min && temp->checked == 0)
-        {   
-            temp->content = index;
-            temp->checked = 1;
-            break;
-        }
-        temp = temp->next;
-    }
-    temp = *a;
-    while (temp)
-    {
-        if (temp->checked == 0)
-        {
-            make_positive(a, ++index);
-            return ;
-        }
-        temp = temp->next;
-    }
-}
-*/
 
 void	right_shift(t_list **a)
 {
-	t_list	*temp;
-	
-	temp = *a;
-	while (*a)
-	{
-		(*a) -> content = ((*a) -> content >> 1);
-		*a = (*a) -> next;
-	}
-    *a = temp;
+	 t_list *temp = *a;
+
+    while (temp)
+    {
+        temp->content = (temp->content >> 1);
+        temp = temp->next;
+    }
 }
 
 int	has_lastbit_zero(t_list *a)
@@ -88,7 +58,6 @@ int all_zero(t_list *a)
 void	radix_sort(t_list **a, t_list **b) // WHY DONT YOU WORK?
 {
     //make_positive(a);
-   int top;
    t_list   *temp;
    
 	while(find_max(*a))
@@ -97,10 +66,7 @@ void	radix_sort(t_list **a, t_list **b) // WHY DONT YOU WORK?
         while (temp != NULL && temp -> content % 2 == 0)
             temp = temp -> next;
         if (temp != NULL)
-            top = temp -> content;
-        else
-            top = 0;
-        
+            temp -> checked = 1;
 		while (has_lastbit_zero(*a))
         {
             //TO DO: Stop if a is already sorted
@@ -109,13 +75,10 @@ void	radix_sort(t_list **a, t_list **b) // WHY DONT YOU WORK?
 			else
 				rotate(a, 'a');
         }
-        //printf("%d ", top);
-        if (top != 0){
-            while ((*a)->content != top){
-                
+        if (temp != NULL){
+            while ((*a)->checked != 1)  
                 rotate(a, 'a');
-            }
-        }
+        (*a)->checked = 0;}
 		while (ft_lstsize(*b) > 0)
 			push(b, a, 'a');
         right_shift(a);
